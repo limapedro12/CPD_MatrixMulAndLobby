6bd62a0d06b7f4915a8fc270a19b792243ae6736
Pedro de Almeida Lima <up202108806@fe.up.pt>
2024-05-05 21:14:16 +0100

    Thinking about rank algorithm part 3

--- assign2/src/server/lobby/RankLobby.java
+++ assign2/src/server/lobby/RankLobby.java
@@ -69,14 +69,6 @@ public class RankLobby implements Runnable, Lobby {
                 timeWaiting.set(i, timeWaiting.get(i) + 1);
             }
 
-            // P:2000-R:300  P:1000-R:750  P:70-R:10  P:50-R:50  P:10-R:1  P:5-R:1
-            // 
-            // 2300-0 1700-0 1750-1 350-1 80-2 60-2 100-3 0-3 11-4 9-4 6-5 4-5
-            // 
-            // 2300-0 1750-1 1700-0 350-1 100-3 80-2 60-2 11-4 9-4 6-5 4-5 0-3
-            // |------------------------| |----------------------------------|
-            //
-            // {0, 1}, {2, 3, 4, 5}
 
             List<Pair<Integer, Integer>> listOrdered = new ArrayList<>();
 
@@ -88,43 +80,7 @@ public class RankLobby implements Runnable, Lobby {
                 listOrdered.add(new Pair<>(minPoints, i));
             }
 
-            // List<Triplet<Integer, Integer, String>> listOrdered = new ArrayList<>();
-
-            // for (int i = 0; i < playersWaiting.size(); i++) {
-            //     int playersPoints = playersWaiting.get(i).getPoints();
-            //     int maxPoints = playersPoints + timeWaiting.get(i) * step;
-            //     int minPoints = playersPoints - timeWaiting.get(i) * step;
-            //     listOrdered.add(new Triplet<>(maxPoints, i, "max"));
-            //     listOrdered.add(new Triplet<>(minPoints, i, "min"));
-            //     listOrdered.add(new Triplet<>(playersPoints, i, "center"));
-            // }
-
             listOrdered.sort((a, b) -> b.first - a.first);
-
-            // Set<Set<Integer>> playerSets = new HashSet<>();
-            // Set<Integer> currentSet = new HashSet<>();
-            // Set<Integer> tempSet = new HashSet<>();
-            // for(Pair<Integer, Integer> pair : listOrdered) {
-            //     if(tempSet.isEmpty()){
-            //         if(!currentSet.isEmpty()){
-            //             playerSets.add(new HashSet<>(currentSet));
-            //             currentSet.clear();
-            //         }
-            //         tempSet.add(pair.second);
-            //         currentSet.add(pair.second);
-            //     } else {
-            //         if(tempSet.contains(pair.second)) {
-            //             tempSet.remove(pair.second);
-            //         } else {
-            //             tempSet.add(pair.second);
-            //             currentSet.add(pair.second);
-            //         }
-            //     }
-            // }
-
-    //         // if(!currentSet.isEmpty()){
-    //         //     playerSets.add(new HashSet<>(currentSet));
-    //         // }
 
             Set<Set<Integer>> playerSets = new HashSet<>();
             Set<Integer> currentOpen = new HashSet<>();
@@ -141,29 +97,6 @@ public class RankLobby implements Runnable, Lobby {
                 }
             }
 
-    //         // P:2000-R:300  P:1750-R:750  P:70-R:10  P:50-R:50  P:30-R:30 P:30-R:1  P:5-R:1
-    //         // 
-    //         // 2300-0-max 2000-0-center 1700-0-min 
-    //         // 2500-1-max 1750-1-center 1000-1-min 
-    //         // 80-2-max 70-2-center 60-2-min 
-    //         // 100-3-max 50-3-center 0-3-min
-    //         // 60-4-max 30-4-center 0-4-min
-    //         // 31-5-max 30-5-center 29-5-min 
-    //         // 6-6-max 5-6-center 4-6-min
-    //         // 
-    //         // 2500-1-max 2300-0-max 2000-0-center
-    //         // 1750-1-center 1700-0-min 1000-1-min
-    //         // 100-3-max 80-2-max 70-2-center
-    //         // 60-4-max 60-2-min 50-3-center
-    //         // 31-5-max 30-4-center 30-5-center
-    //         // 29-5-min 6-6-max 5-6-center
-    //         // 4-6-min 0-3-min 0-4-min
-    //         // 
-    //         // currOpen: 3, 4
-    //         // {0, 1}, {1, 0}, {2}, {3, 2, 4, 5, 6}, {4, 3, 5, 6}, {5, 4}, {6}
-    //         //
-    //         // {0, 1}, {2}, {3,4}, {4,5}, {6}
-
             List<Set<Integer>> playerSets = new ArrayList<>();
             Set<Integer> currentOpen = new HashSet<>();
 
@@ -173,9 +106,6 @@ public class RankLobby implements Runnable, Lobby {
 
             for(Triplet<Integer, Integer, String> triplet : listOrdered) {
                 if(triplet.third.equals("center")) {
-                    // for(int i : currentOpen) {
-                    //     playerSets.get(i).add(triplet.second)
-                    // }
                     playerSets.set(triplet.second, new HashSet<>(currentOpen));
                 } else if(triplet.third.equals("max")) {
                     currentOpen.add(triplet.second);
@@ -184,30 +114,25 @@ public class RankLobby implements Runnable, Lobby {
                 }
             }
 
-    //         for(Set<Integer> set : playerSets) {
-    //             if(set.size() == numPlayers) {
-    //                 Set<Player> players = new HashSet<>();
-    //                 for(Integer i : set) {
-    //                     players.add(playersWaiting.get(i));
-    //                 }
-    //                 new Thread(new Game(players)).start();
-    //                 for(int i : set) {
-    //                     playersWaiting.remove(i);
-    //                     timeWaiting.remove(i);
-    //                 }
-    //             }
-    //         } 
-
-    //         if(show){
-    //             if(!playersWaiting.isEmpty()){
-    //                 System.out.println("List Ordered: " + listOrdered);
-    //                 System.out.println("Player Sets: " + playerSets);
-    //             }
-    //         }
-
-    //         lock.unlock();  
-    //     }
-    // }
+            for(Set<Integer> set : playerSets) {
+                if(set.size() == numPlayers) {
+                    List<Player> players = new ArrayList<>();
+                    for(Integer i : set) {
+                        players.add(playersWaiting.get(i));
+                    }
+                    Game game = new Game(players);
+                    game.start();
+                    for(Integer i : set) {
+                        playersWaiting.remove(i);
+                        timeWaiting.remove(i);
+                        for(Set<Integer> set : playerSets) {
+                            set.remove(i);
+                        }
+                    }
+                }
+            }
+        }
+    }
 
     public void removePlayer(Player player) {
         lock.lock();
